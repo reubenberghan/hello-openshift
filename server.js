8502de26b46b3f8fd04965d3e52f41a99ea234bc
Reuben Berghan <reuben.berghan@gmail.com>
2019-07-28 17:03:27 +1200

    refactor: add port env var

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http')
 
+const PORT = process.env.PORT || 3000
+
 const server = http.createServer((req, res) => {
   res.end('hello openshift')
 })
 
-server.listen(3000, () => console.log('Server up and running on port 3000!'))
+server.listen(PORT, () => console.log('Server up and running on port 3000!'))
